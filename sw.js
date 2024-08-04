8fc34f983637b8e3c0108f7308239838bb10428d
c-py <41215134+c-py@users.noreply.github.com>
2024-08-04 15:17:37 +1000

    send opaque response

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 async function exampleCom(request) {
-  return fetch("https://example.com");
+  return fetch("https://example.com", {
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
 
 self.addEventListener("fetch", function (e) {
